50abfe51893721946b43f7560d6b490eebb79c9e
abosboss <ahmedyusuff@gmail.com>
2020-03-26 21:57:45 +0100

    new commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let name;
+name = 'Yusuff Ahmed';
+var courses = ["Html", "css","JavaScript"]; 
+console.log(name);
+console.log(courses);
+for(i=1; i<=200; i++){ 
+    if(i === 0)
+        console.log(i);
+    
+}
